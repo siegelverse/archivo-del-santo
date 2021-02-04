8dca487e188bda11b3d980379b4febce9bc28b7e
siegelverse <msiegel37@gmail.com>
2021-02-04 15:35:01 -0600

    prod build

--- db/index.js
+++ db/index.js
@@ -3,6 +3,7 @@ require('./mongoose')
 const cors = require('cors')
 const userRouter = require('./routers/user')
 const reviewRouter = require('./routers/review')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT
@@ -21,6 +22,14 @@ app.use(express.json())
 app.use(cors())
 app.use(userRouter)
 app.use(reviewRouter)
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('/buld'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    })
+}
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`)
